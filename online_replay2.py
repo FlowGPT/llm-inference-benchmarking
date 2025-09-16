fbefc07471f546f6d953e6a9b48e5d6a0241f519
qian.wang <>
2025-09-16 13:02:35 +0800

    fix

--- online_replay2.py
+++ online_replay2.py
@@ -209,7 +209,7 @@ def process_log_line(line: str, sample_start: float = 0.0, sample_end: float = 1
 
         messages = request_data['body'].get('prompt')
         tokens_count = count_tokens(messages)
-        if tokens_count < 4100 or tokens_count > 4500:
+        if tokens_count < 3800 or tokens_count > 4200:
             return None
 
         # 构造请求体
